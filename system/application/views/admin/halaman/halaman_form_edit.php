bbf2e8fbd5f4ae19d252a4a241e4471543cad441
nhatphong38 <nhatphong38@25e765e2-cf0d-8086-04ae-b7e6ce210afa>
2011-03-13 09:35:22 +0000

    add module for pages

--- system/application/views/admin/halaman/halaman_form_edit.php
+++ system/application/views/admin/halaman/halaman_form_edit.php
@@ -134,6 +134,7 @@ Edit
 	{
 	?>
 		<select name="page_module">
+		<option value="services" <?php if ($single_record_module == 'services') echo 'selected'; else echo '' ?>>Services</option>	
 		<option value="gallery" <?php if ($single_record_module == 'gallery') echo 'selected'; else echo '' ?>>Gallery</option>
 		<option value="guestbook" <?php if ($single_record_module == 'guestbook') echo 'selected'; else echo '' ?>>Guest Book</option>
 		<option value="polling" <?php if ($single_record_module == 'polling') echo 'selected'; else echo '' ?>>Polls</option>
